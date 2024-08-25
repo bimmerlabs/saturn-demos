b32ec47f386810d0b659b9e0d05e76ce5c943b7c
maschine <hassmaschine79@gmail.com>
2024-08-25 12:38:07 -0700

    Update compile.bat

--- demo - HSL color calc/compile.bat
+++ demo - HSL color calc/compile.bat
@@ -1,5 +1,6 @@
 @ECHO Off
 SET COMPILER_DIR=..\..\Compiler
 SET PATH=%COMPILER_DIR%\WINDOWS\Other Utilities;%COMPILER_DIR%\WINDOWS\bin;%PATH%
-perl "M:\Saturn\Development\_Tools\TGA_Tools\tga_tools.pl"
+REM uncomment and update path to auto-build your palettes etc:
+REM perl "path-to\tga_tools.pl"
 make re
