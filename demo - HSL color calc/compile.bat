6f8d2362abc8d8b791087019f89a682cc4a8b95f
maschine <hassmaschine79@gmail.com>
2024-09-05 09:11:29 -0700

    Update compile.bat
    Add options for image name & project path

--- demo - HSL color calc/compile.bat
+++ demo - HSL color calc/compile.bat
@@ -2,5 +2,5 @@
 SET COMPILER_DIR=..\..\Compiler
 SET PATH=%COMPILER_DIR%\WINDOWS\Other Utilities;%COMPILER_DIR%\WINDOWS\bin;%PATH%
 REM uncomment and update path to auto-build your palettes etc:
-REM perl "path-to\tga_tools.pl"
+REM perl "path-to\tga_tools.pl" --p "path-to-project\joengine-master\Projects\demo - HSL color calc" --in_image IMAGENAME
 make re
